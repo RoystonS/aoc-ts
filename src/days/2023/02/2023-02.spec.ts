b93141e63e8c5419aa18d606cc1805f2adb3ae5c
Royston Shufflebotham <royston@shufflebotham.org>
2023-12-02 12:30:59 +0000

    2023 day 2

---
+++ src/days/2023/02/2023-02.spec.ts
@@ -0,0 +1,58 @@
+import { Game, parseLine, puzzle } from "./Puzzle";
+import { readInput } from "../../../common";
+
+const year = "2023";
+const day = "02";
+
+describe("2023-02", () => {
+  test("parseLine", () => {
+    const result = parseLine(
+      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    );
+    const expected: Game = {
+      id: 5,
+      handfulls: [
+        {
+          sets: new Map([
+            ["red", 6],
+            ["blue", 1],
+            ["green", 3],
+          ]),
+        },
+        {
+          sets: new Map([
+            ["blue", 2],
+            ["red", 1],
+            ["green", 2],
+          ]),
+        },
+      ],
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  const example = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+  it("passes part1 example", () => {
+    expect(puzzle.computePart1(example)).toEqual("8");
+  });
+
+  it("passes part1", () => {
+    const input = readInput(year, day);
+    expect(puzzle.computePart1(input)).toEqual(puzzle.part1Answer);
+  });
+
+  it("passes part2 example", () => {
+    expect(puzzle.computePart2(example)).toEqual("2286");
+  });
+
+  it("passes part2", () => {
+    const input = readInput(year, day);
+    expect(puzzle.computePart2(input)).toEqual(puzzle.part2Answer);
+  });
+});
---
+++ src/days/2023/02/Puzzle.ts
@@ -0,0 +1,85 @@
+import { assertDefinedAndNotNull, splitLines } from "../../../common";
+import type { Puzzle } from "../../../common";
+
+export const puzzle: Puzzle = {
+  computePart1(input) {
+    const lines = splitLines(input);
+    const games = lines.map(parseLine);
+
+    let idTotal = 0;
+    for (const game of games) {
+      if (
+        game.handfulls.every((handfull) => {
+          const redCount = handfull.sets.get("red") ?? 0;
+          const greenCount = handfull.sets.get("green") ?? 0;
+          const blueCount = handfull.sets.get("blue") ?? 0;
+
+          return redCount <= 12 && greenCount <= 13 && blueCount <= 14;
+        })
+      ) {
+        idTotal += game.id;
+      }
+    }
+    return idTotal.toString();
+  },
+  part1Answer: "2439",
+
+  computePart2(input) {
+    const lines = splitLines(input);
+    const games = lines.map(parseLine);
+
+    const powerTotal = games.reduce((acc, game) => {
+      // Compute the minimum cubes for this game
+      const min = new Map<CubeColour, number>();
+      for (const handful of game.handfulls) {
+        for (const [colour, count] of handful.sets) {
+          const existingMin = min.get(colour) ?? 0;
+          if (count > existingMin) {
+            min.set(colour, count);
+          }
+        }
+      }
+
+      // Multiply the cube counts together
+      const gamePower = Array.from(min.values()).reduce((acc, n) => acc * n, 1);
+      return gamePower + acc;
+    }, 0);
+    return powerTotal.toString();
+  },
+  part2Answer: "63711",
+};
+
+// Game N: X blue, Y red; Z green, A blue
+export function parseLine(line: string): Game {
+  const gameMatch = line.match(/Game (\d+): (.*)/);
+  assertDefinedAndNotNull(gameMatch);
+  const [, gameIdString, restOfLine] = gameMatch;
+  const gameId = parseInt(gameIdString, 10);
+
+  const handfullsStrings = restOfLine.split("; ");
+  const handfulls: Handfull[] = handfullsStrings.map((handfullString) => {
+    const setsStrings = handfullString.split(", ");
+
+    const map = new Map<CubeColour, number>();
+
+    const sets = setsStrings.reduce<Map<CubeColour, number>>((m, setString) => {
+      const [countString, colourString] = setString.split(" ");
+      m.set(colourString as CubeColour, parseInt(countString, 10));
+      return m;
+    }, map);
+
+    return { sets } satisfies Handfull;
+  });
+
+  return { id: gameId, handfulls } as Game;
+}
+
+type CubeColour = "red" | "green" | "blue";
+
+export interface Game {
+  id: number;
+  handfulls: Handfull[];
+}
+export interface Handfull {
+  sets: Map<CubeColour, number>;
+}
