521bd8d53c241b7096dee7e168399ae5614f07bb
Royston Shufflebotham <royston@shufflebotham.org>
2023-12-06 13:58:57 +0000

    2023 day 6

---
+++ src/days/2023/06/2023-06.spec.ts
@@ -0,0 +1,27 @@
+import { puzzle } from "./Puzzle";
+import { readInput } from "../../../common";
+
+const year = "2023";
+const day = "06";
+
+describe("2023-06", () => {
+  const example = readInput(year, day, "example.txt");
+
+  it("passes part1 example", () => {
+    expect(puzzle.computePart1(example)).toEqual("288");
+  });
+
+  it("passes part1", () => {
+    const input = readInput(year, day);
+    expect(puzzle.computePart1(input)).toEqual(puzzle.part1Answer);
+  });
+
+  it("passes part2 example", () => {
+    expect(puzzle.computePart2(example)).toEqual("71503");
+  });
+
+  it("passes part2", () => {
+    const input = readInput(year, day);
+    expect(puzzle.computePart2(input)).toEqual(puzzle.part2Answer);
+  });
+});
---
+++ src/days/2023/06/Puzzle.ts
@@ -0,0 +1,41 @@
+import { count, filter, map, product, range, splitLines } from "../../../common";
+import type { Puzzle } from "../../../common";
+
+export const puzzle: Puzzle = {
+  computePart1(input) {
+    const lines = splitLines(input);
+
+    const times = Array.from(lines[0].matchAll(/(\d+)/g)).map((m) => parseInt(m[0], 10));
+    const distances = Array.from(lines[1].matchAll(/(\d+)/g)).map((m) => parseInt(m[0], 10));
+
+    const waysToBeat = times.map((time, i) => computeWaysToBeatRecord(time, distances[i]));
+
+    return product(waysToBeat.values()).toString();
+  },
+
+  part1Answer: "275724",
+
+  computePart2(input) {
+    return this.computePart1(input.replaceAll(" ", ""));
+  },
+
+  part2Answer: "37286485",
+};
+
+function computeWaysToBeatRecord(raceTime: number, recordDistance: number) {
+  let waysToBeat = 0;
+
+  for (let i = 0; i <= raceTime; i++) {
+    const distance = computeDistance(raceTime, i);
+    if (distance > recordDistance) {
+      waysToBeat++;
+    }
+  }
+
+  return waysToBeat;
+}
+
+function computeDistance(raceTime: number, holdTime: number) {
+  const runTime = raceTime - holdTime;
+  return runTime * holdTime;
+}
